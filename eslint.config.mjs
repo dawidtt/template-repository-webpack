600935b693fe7748ad4b9129a1a4c62907cfdd29
dawidtt <dawid.gorszka@gmail.com>
2024-10-07 18:22:51 +0200

    Fix eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-import someConfig from "some-other-config-you-use";
-import eslintConfigPrettier from "eslint-config-prettier";
-
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  {
-    extends: ["some-other-config-you-use", "prettier"],
-  },
-  someConfig,
-  eslintConfigPrettier,
 ];
